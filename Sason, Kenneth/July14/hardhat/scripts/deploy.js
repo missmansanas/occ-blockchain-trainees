1a4990e48f25d10d95f6c403c7454d38fde3c22e
kreytra17 <sason.kenneth11@gmail.com>
2023-07-14 23:53:25 +0800

    added activity for miss princess' activity

---
+++ Sason, Kenneth/July14/hardhat/scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  const KenNFTContract = await hre.ethers.deployContract("KenNFT", [
+    "ipfs://Qme6zq49JyS4Tu7iKiQ7VBuLfuyFt6zKRHwAZyAqb6AEoY/0",
+  ]);
+  await KenNFTContract.waitForDeployment();
+  console.log(`Contract Deployed to :${KenNFTContract.target}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
